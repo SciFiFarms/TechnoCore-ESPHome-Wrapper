9500751a23e98a9a2720eefeb81616c421cd36ac
Spencer Hachmeister <SpencerHachmeister@gmail.com>
2019-02-06 20:32:24 -0700

    Allow usage of volume to be overridden with a local mount.

--- main.sh
+++ main.sh
@@ -1,10 +1,18 @@
 #!/usr/bin/env bash
+if [ -d /config/ ]
+then
+    volume=${host_working_dir}/esphomeyaml/config/:/config
+else
+    volume=${stack_name}_${service_name}:/config 
+fi
+echo "Volume: $volume"
+
 docker run --rm --name ${stack_name}_${service_name} \
     -t --network ${stack_name}_web \
     -e CA="$(cat /run/secrets/ca)" \
     -e MQTT_USERNAME="$(cat /run/secrets/mqtt_username)" \
     -e MQTT_PASSWORD="$(cat /run/secrets/mqtt_password)" \
     -e VAULT_TOKEN="$(cat /run/secrets/token)" \
-    -v ${stack_name}_${service_name}:/config \
+    -v $volume \
     --privileged \
     ${image_provider}/technocore-${service_name}:${TAG}
