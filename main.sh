b673d1ed717e4f8575bd1bc1860ce1aa9e761a4c
Spencer Hachmeister <SpencerHachmeister@gmail.com>
2019-11-03 21:33:09 -0700

    Move Traefik labels in to main.sh.

--- main.sh
+++ main.sh
@@ -1,9 +1,36 @@
 #!/usr/bin/env bash
+volume=${TECHNOCORE_ROOT}/hals/:/config
+
+# This allows us to pass the envs set in the compose.yml file into the wrapped service.
+# https://askubuntu.com/questions/275965/how-to-list-all-variables-names-and-their-current-values
+env_vars=
+for env in $(declare -xpn | cut -d " " -f 3- | cut -d "=" -f 1 | \
+        grep -v HOME | \
+        grep -v HOSTNAME | \
+        grep -v OLDPWD | \
+        grep -v PATH | \
+        grep -v PWD | \
+        grep -v SHLVL | \
+        grep -v TERM 
+    ); do
+    env_vars="${env_vars} -e ${env}=\"${!env}\""
+done
 docker run --rm --name ${STACK_NAME}_${service_name}_app \
     -t \
     --network ${STACK_NAME}_${service_name} \
+    $env_vars \
     -v $volume \
     -v ${host_working_dir}/$esphome_core \
     -v ${host_working_dir}/$esphome_app \
+    -l traefik.frontend.rule=$ESPHOME_ROUTING_LABEL \
+    -l traefik.frontend.priority=$ESPHOME_ROUTING_LABEL_PRIORITY \
+    -l traefik.port=${SERVICE_PORT} \
+    -l traefik.enable=true \
+    -l traefik.tags=ingress  \
+    -l traefik.docker.network=${STACK_NAME:-technocore}_${service_name} \
+    -l traefik.redirectorservice.frontend.entryPoints=http \
+    -l traefik.redirectorservice.frontend.redirect.entryPoint=https \
+    -l traefik.webservice.frontend.entryPoints=https \
+    -l com.ouroboros.enable=true \
     --privileged \
     ${image_provider}/technocore-${service_name}:${TAG}
