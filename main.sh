61e426873b1e0604be403ff4ff17decf2a4f490a
Spencer Hachmeister <SpencerHachmeister@gmail.com>
2019-11-11 17:55:50 -0700

    Add secrets to env auto expansion to main.sh.

--- main.sh
+++ main.sh
@@ -5,6 +5,52 @@ volume=${TECHNOCORE_ROOT}/hals/:/config
 # https://stackoverflow.com/questions/46099874/how-can-i-forward-a-port-from-one-docker-container-to-another
 socat TCP-LISTEN:${SERVICE_PORT},fork TCP:${STACK_NAME}_${service_name}_app:${SERVICE_PORT} &
 
+# Most of this file comes from https://medium.com/@basi/docker-environment-variables-expanded-from-secrets-8fa70617b3bc 
+# Thanks Basilio Vera, Rubén Norte, and Jose Manuel Cardona! 
+
+: ${ENV_SECRETS_DIR:=/run/secrets}
+
+env_secret_debug()
+{
+    if [ ! -z "$ENV_SECRETS_DEBUG" ]; then
+        echo -e "\033[1m$@\033[0m"
+    fi
+}
+
+# usage: env_secret_expand VAR
+#    ie: env_secret_expand 'XYZ_DB_PASSWORD'
+# (will check for "$XYZ_DB_PASSWORD" variable value for a placeholder that defines the
+#  name of the docker secret to use instead of the original value. For example:
+# XYZ_DB_PASSWORD={{DOCKER-SECRET:my-db.secret}}
+env_secret_expand() {
+    var="$1"
+    eval val=\$$var
+    if secret_name=$(expr match "$val" "{{DOCKER-SECRET:\([^}]\+\)}}$"); then
+        secret="${ENV_SECRETS_DIR}/${secret_name}"
+        env_secret_debug "Secret file for $var: $secret"
+        if [ -f "$secret" ]; then
+            val=$(cat "${secret}")
+            export "$var"="$val"
+            env_secret_debug "Expanded variable: $var=$val"
+        else
+            env_secret_debug "Secret file does not exist! $secret"
+        fi
+    fi
+}
+
+env_secrets_expand() {
+    for env_var in $(printenv | cut -f1 -d"=")
+    do
+        env_secret_expand $env_var
+    done
+
+    if [ ! -z "$ENV_SECRETS_DEBUG" ]; then
+        echo -e "\n\033[1mExpanded environment variables\033[0m"
+        printenv
+    fi
+}
+env_secrets_expand
+
 # This allows us to pass the envs set in the compose.yml file into the wrapped service.
 # https://askubuntu.com/questions/275965/how-to-list-all-variables-names-and-their-current-values
 env_vars=
@@ -17,8 +63,12 @@ for env in $(declare -xpn | cut -d " " -f 3- | cut -d "=" -f 1 | \
         grep -v SHLVL | \
         grep -v TERM 
     ); do
-    env_vars="${env_vars} -e ${env}=\"${!env}\""
+# TODO: This doesn't do a good job of passing env's with spaces in their value.
+# "s don't seem to get passed in correctly. Not yet set. is one to break the service.
+# https://stackoverflow.com/questions/46141148/declare-env-variable-which-value-include-space-for-docker-docker-compose
+env_vars="${env_vars} -e ${env}=${!env}"
 done
+
 docker run --rm --name ${STACK_NAME}_${service_name}_app \
     -t \
     --network ${STACK_NAME}_${service_name} \
